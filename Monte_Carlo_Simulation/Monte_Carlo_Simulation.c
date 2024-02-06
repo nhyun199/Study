51334bc89be2e8b0ca6f51c72257fc8fc6e7e2b4
nhyun199 <shgus1692@gmail.com>
2024-02-06 17:28:41 +0900

    C Practice
    - C practice
    - Operation and Random num generate

---
+++ Monte_Carlo_Simulation/Monte_Carlo_Simulation.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TRIALS 1000000
+
+int main()
+{
+	srand(time(0)); // 시드 초기화
+
+	int cnt[13] = { 0 };
+	int rand_min = 1;
+	int rand_max = 6;
+	int dice;
+	
+	int v = rand_max - rand_min + 1;
+
+	for (int i = 0; i < TRIALS; i++)
+	{
+		dice = (int)((double)rand() / ((RAND_MAX + 1)) * v + rand_min);
+		dice += (int)((double)rand() / ((RAND_MAX + 1)) * v + rand_min); // 두 주사위의 합
+		cnt[dice]++;
+	}
+
+	int total = 0;
+	for (int i = 2; i <= 12; i++)
+	{
+		printf("cnt[%2d] = %8d, %5.2f%%\n", i, cnt[i], (double)cnt[i] / TRIALS * 100);
+		total += cnt[i];
+	}
+	printf("total = %d\n", total);
+
+}
---
+++ Randam_num_example/Randam_num.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	srand(time(0));
+
+	int a[10]; // 난수 생성 테스트 배열
+	int Dice[30]; // 주사위를 30번 던졌을 때 나오는 수를 저장할 배열
+	int randmax = 6; // 난수의 최대값
+	int randmin = 1; // 난수의 최소값
+
+	for (int i = 0; i < 10; i++)
+	{
+		a[i] = rand();
+	}
+
+	printf("RAND_MAX = %x, %d\n", RAND_MAX, RAND_MAX);
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%6d", a[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < 30; i++)
+	{
+		//Dice[i] = (double)rand() / RAND_MAX * (randmax + randmin - 1) + randmin;
+		Dice[i] = (rand() % (randmax - randmin + 1)) + randmin;
+	}
+
+	for (int i = 0; i < 30; i++)
+	{
+		printf("%6d%c", Dice[i], (i + 1) % 10 != 0 ? ' ' : '\n');
+	}
+}
