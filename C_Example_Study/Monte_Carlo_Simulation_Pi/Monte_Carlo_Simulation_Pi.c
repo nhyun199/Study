dbdc7518aa634e2351aa0976649d0ee611f3d2d4
nhyun199 <shgus1692@gmail.com>
2024-02-07 16:47:30 +0900

    Randam Num & Double Repeation
    - Monte Carlo Smulation.
    - Double repeation.

---
+++ C_Example_Study/Monte_Carlo_Simulation_Pi/Monte_Carlo_Simulation_Pi.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    // 반지름이 r인 원 , 넓이 Pi * r^2
+    // 원을 둘러싼 정사각형 한 변 2r, 넓이 : 4r^2
+    // 사각형 / 원 = 4 / Pi
+
+    int x, y; // 점 x, y
+    int icnt = 0; // 원 내부 점의 개수
+    int ocnt = 0; // 원 외부 점의 개수
+    int rad = 50; // 반지름의 길이
+    double pi; // 원주율 -> 이걸 구해야함
+
+    for (int i = 1; i <= 100000000; i++)
+    {
+        x = rand() % 100;
+        y = rand() % 100;
+
+        if ((x - rad)*(x - rad) + (y - rad)*(y - rad) <= rad * rad)
+        {
+            icnt++;
+        }
+        else
+        {
+            ocnt++;
+        }
+
+        pi = (double)icnt / (icnt + ocnt) * 4;
+
+        if (i < 10 || i > 99999990)
+            printf("i = %d, pi = %.9f\n", i, pi);
+    }
+
+}
---
+++ C_Example_Study/Pi/Pi.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool sign = false; // 조건에 사용할 변수 sign
+    double Pi = 0;     // Pi의 값
+    
+    for (int i = 1; i < 100000000; i += 2)
+    {
+        if (sign == false)
+        {
+            Pi += 1.0 / i;
+            sign = true;
+        }
+        else
+        {
+            Pi -= 1.0 / i;
+            sign = false;
+        }
+
+        if (i < 20 || i > 99999990) // 모든 결과를 볼 수는 없으므로 보여줄 구간을 설정함
+            printf("i = %d, Pi = %.9f\n", i, 4 * Pi);
+    }
+
+    
+}
+
+
