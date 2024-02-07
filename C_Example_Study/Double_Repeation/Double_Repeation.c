ca9a46b3f2b668a13a42c6245e6ac50b86588f38
nhyun199 <shgus1692@gmail.com>
2024-02-07 16:47:30 +0900

    Randam Num & Double Repeation & Enum
    - Monte Carlo Smulation.
    - Enum
    - Double repeation.

---
+++ C_Example_Study/Double_Repeation/Double_Repeation.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    // * 을 5줄 출력
+    // * 이 줄바꿈마다 1개씩 증가
+     
+    for (int i = 1; i <= 5; i++)
+    {        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");            
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 0; j < 6 - i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i <= 5; i++)
+    {
+        
+    }
+
+}
---
+++ C_Example_Study/Enum_Price/Enum_Price.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+// 커피가격을 enum을 통해 출력하기
+// 아메리카노 short 3800 tall 4100 grande 4600 venti 5100
+// 카푸치노 4600 5900 6400 6900
+
+enum Size {Short, Tall, Grande, Venti};
+char size_name[][7] = { "Short", "Tall", "Grande", "Venti" };
+int price_Americano[] = { 3800, 4100, 4600, 5100 };
+int price_Cappuccino[] = { 4600, 5900, 6400, 6900 };
+
+int main() 
+{
+    printf("아메리카노\n");
+    for (int i = Short; i <= Venti; i++)
+    {
+        printf("%7s : %5d원\n", size_name[i], price_Americano[i]);
+    }
+    printf("\n");
+
+    printf("카푸치노\n");
+    for (int i = Short; i <= Venti; i++)
+    {
+        printf("%7s : %5d원\n", size_name[i], price_Cappuccino[i]);
+    }
+}
